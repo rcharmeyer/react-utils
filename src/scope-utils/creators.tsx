c8c8850b61e39210d0e8cc3590a77e6bbbcdf32e
rcharmeyer <rcharmeyer@hotmail.com>
2023-04-02 12:26:28 -0700

    v0.0.6 Update storeFamily and hoist for optional deps

--- src/scope-utils/creators.tsx
+++ src/scope-utils/creators.tsx
@@ -8,14 +8,14 @@ import {
 
 type Func = (...args: any[]) => any
 
-export function createStoreFamily <T extends Func> (hook: T, deps: Scope[]) {
+export function createStoreFamily <T extends Func> (hook: T, deps?: Scope[]) {
   return memoize ((...args: Parameters <T>) => {
     const res = createStore (() => hook (...args), deps)
     return res as Store <ReturnType <T>>
   })
 }
 
-export function hoist <T extends Func> (hook: T, deps: Scope[]) {
+export function hoist <T extends Func> (hook: T, deps?: Scope[]) {
   const storeFamily = createStoreFamily <T> (hook, deps)
   return (...args: Parameters <T>): ReturnType <T> => {
     const store = storeFamily (...args)
